e894e491c8dcb9a7c0a5957a16ba86c08a3b36d5
keenwarrior <anujgargcse@gmail.com>
2024-03-02 11:59:12 +0530

    razorpay

---
+++ razorpay-demo/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+import useRazorpay from "react-razorpay";
+
+
+
+function App() {
+
+  const [Razorpay] = useRazorpay();
+
+  const handlePayment = async () => {
+    const options = {
+      order_id: "order_NhNzpJGcdPxwMi"
+    }
+
+    const paymentObject = new Razorpay(options);
+
+    paymentObject.open();
+
+  }
+
+  return (
+    <div className="App">
+     <button onClick={handlePayment}>Click to pay</button>
+    </div>
+  );
+}
+
+export default App;
